2f51f74223b053620b249fe7a2b27db205ccafed
bart <zibadian@gmail.com>
2016-05-21 21:30:46 +0200

    Application version fix and improvement in converter and interceptor configuration.

--- commons/src/main/java/nl/cap/csd/capbox/commons/config/CommonsConfiguration.java
+++ commons/src/main/java/nl/cap/csd/capbox/commons/config/CommonsConfiguration.java
@@ -12,9 +12,13 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 @Configuration
 @PropertySource("classpath:/application.properties")
@@ -48,8 +52,24 @@ public class CommonsConfiguration extends WebMvcConfigurationSupport {
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(customJackson2HttpMessageConverter());
         super.addDefaultHttpMessageConverters(converters);
+        // Replace default Jackson mapping converter with custom converter
+        for (Iterator<HttpMessageConverter<?>> i = converters.iterator(); i.hasNext();) {
+            final HttpMessageConverter<?> converter = i.next();
+            if (converter instanceof MappingJackson2HttpMessageConverter) {
+                i.remove();
+            }
+        }
+        converters.add(customJackson2HttpMessageConverter());
+    }
+
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        final Map<String, HandlerInterceptor> interceptors = getApplicationContext().getBeansOfType(HandlerInterceptor.class);
+        for (HandlerInterceptor interceptor : interceptors.values()) {
+            registry.addInterceptor(interceptor);
+        }
+        super.addInterceptors(registry);
     }
 
 }
